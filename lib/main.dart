e0fe0277425a9dd155c181278ede20c77c0c7b9c
SANJITH KUMAR R <sanjithkumar986@gmail.com>
2024-01-08 23:03:16 +0530

    state_less

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text('My Simple App'),
-      centerTitle: true,
-      foregroundColor: Colors.blueGrey,
-      backgroundColor: Colors.red[400],
-    ),
-    body: const Center(
-        child: Text("Hello Everyone",
-        style: TextStyle(
-          backgroundColor: Colors.black26,
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
-          fontFamily: 'Oswald',
-          color: Colors.grey[600],
-        ),),
-    ),
-    floatingActionButton: const FloatingActionButton(
-      onPressed: () {},
-      backgroundColor: Colors.red[400],
-      child: Text('click'),
-    ),
-  ),
+void main() => runApp(const MaterialApp(
+  home: Home(),
 ));
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Simple App'),
+        centerTitle: true,
+        foregroundColor: Colors.blueGrey,
+        backgroundColor: Colors.red[400],
+      ),
+      body: const Center(
+        child: Icon(
+         Icons.airline_seat_flat,
+         color: Colors.black,
+         size: 30,
+        ),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.red[400],
+        child: Text('click'),
+      ),
+    );
+  }
+}
+
 
